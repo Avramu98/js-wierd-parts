55fd41312cab3a037573995a78c91bf693562efa
Avramu98 <vlad_avram98@yahoo.com>
2022-09-26 19:19:34 +0300

    fixed quiz

--- ui-builders/quiz-builder.js
+++ ui-builders/quiz-builder.js
@@ -63,6 +63,7 @@ const quizBuilder = (quizes) => {
       userInputContainerElement.appendChild(checkAnswerButton);
       quizSectionElement.append(questionContainerElement, answerContainerElement, userInputContainerElement);
     } else {
+      quizSectionElement.append(questionContainerElement);
       prepareFrame(codeSnippetFrame, questionContainerElement);
     }
     quizListElement.appendChild(quizSectionElement);
